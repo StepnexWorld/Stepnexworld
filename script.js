fb95e66da99ca724beec51e70afe504891c1f06b
StepnexWorld <Stepnex1995@gmail.com>
2026-07-08 00:30:19 -0700

    Add animation initialization and button hover effects

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// Initialize Animation
+AOS.init({
+    duration:1000,
+    once:true
+});
 
+// Smooth Button Animation
+
+const buttons = document.querySelectorAll(".btn1,.btn2");
+
+buttons.forEach(btn=>{
+
+btn.addEventListener("mouseenter",()=>{
+
+btn.style.transform="scale(1.08)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+btn.style.transform="scale(1)";
+
+});
+
+});
